c8c38dc769cb37be2dc65a1f40542313ba8ff252
TUCCHI1 <tutikikin1@gmail.com>
2022-11-20 12:26:24 +0900

    complete backend

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { Express, Request, Response, urlencoded } from "express";
 
 const express = require("express");
 const app: Express = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 
 const jwt = require("jsonwebtoken");
-const auth = require("./utils/auth");
+const { auth } = require("./utils/auth");
 
 const { connectDB } = require("./utils/database");
 const { ItemModel, UserModel } = require("./utils/schemaModels");
@@ -57,10 +59,16 @@ app.get("/item/:id", async (req: Request, res: Response) => {
 app.put("/item/update/:id", auth, async (req: Request, res: Response) => {
   try {
     await connectDB();
-    await ItemModel.updateOne({ _id: req.params.id }, req.body);
-    return res.status(200).json({
-      message: "アイテム編集成功",
-    });
+    const singleItem = await ItemModel.findById(req.params.id);
+
+    if (singleItem.email === req.body.email) {
+      await ItemModel.updateOne({ _id: req.params.id }, req.body);
+      return res.status(200).json({
+        message: "アイテム編集成功",
+      });
+    } else {
+      throw new Error();
+    }
   } catch (err) {
     return res.status(400).json({
       message: "アイテム編集失敗",
@@ -71,8 +79,14 @@ app.put("/item/update/:id", auth, async (req: Request, res: Response) => {
 app.delete("/item/delete/:id", auth, async (req: Request, res: Response) => {
   try {
     await connectDB();
-    await ItemModel.deleteOne({ _id: req.params.id });
-    return res.status(200).json({ message: "アイテム削除成功" });
+    const singleItem = await ItemModel.findById(req.params.id);
+
+    if (singleItem.email === req.body.email) {
+      await ItemModel.deleteOne({ _id: req.params.id });
+      return res.status(200).json({ message: "アイテム削除成功" });
+    } else {
+      throw new Error();
+    }
   } catch (err) {
     return res.status(400).json({
       message: "アイテム削除失敗",
@@ -134,4 +148,11 @@ app.post("/user/login", async (req: Request, res: Response) => {
 
 app.listen(5000, () => {
   console.log("Listening on localhost port 5000");
+});
+
+// Connecting to port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on localhost port ${port}`);
 });
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,27 @@
-import { NextFunction } from "express";
-
-export const auth = async (req: Request, res: Response, next: NextFunction) => {
+export const auth = async (req: any, res: any, next: any) => {
   if (req.method === "GET") {
     return next();
   }
 
-  const token = await req.headers.autho;
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InR1dGlraWtpbjFAZ21haWwuY29tIiwiaWF0IjoxNjY4OTExMDkzLCJleHAiOjE2Njg5OTM4OTN9.yxoobV2MTjUxjxmn79cjb9UKoxJ7gI2-HuIL7bzDFJw";
+  // const token = await req.headers.authorization.split(" ")[1];
+  const jwt = require("jsonwebtoken");
+  const secret_key = "mern-market";
+
+  if (!token) {
+    return res.status(400).json({
+      message: "トークンがありません",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret_key);
+    req.body.email = decoded.email;
+    return next();
+  } catch (err) {
+    return res.status(400).json({
+      message: "トークンが正しくないので、ログインしてください",
+    });
+  }
 };
